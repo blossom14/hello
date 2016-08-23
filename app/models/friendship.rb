cdea9c1b4fb031da07d67d45966701857a4d2290
blossom14 <wlsdud0581@naver.com>
2016-08-23 15:11:43 +0900

    create friendship scopes(:pending, :accepted)

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,7 @@
 class Friendship < ApplicationRecord
 	belongs_to :user
 	belongs_to :friend, class_name: "User"
+
+	scope :pending, -> { where(accepted: false) }
+	scope :accepted, -> { where(accepted: true) }
 end
